0a6bd79127513fde16f1d59a219ad886de618a3e
coreen <coreeny@cs.washington.edu>
2014-01-03 10:31:40 -0800

    Added test cases, changed function name to repeat.

--- repeat.rkt
+++ repeat.rkt
@@ -1,10 +1,24 @@
 ;; Coreen Yuen
 
 #lang racket
+(require rackunit)
+(require rackunit/text-ui)
 
 ;; function returns #t if there is at least one repeated adjacent element in given list
-(define (twice s)
+(define (repeat s)
   (cond ((null? s) #f)
     ((null? (cdr s)) #f)
     ((equal? (car s) (cadr s)) #t)
-    (else (twice (cdr s)))))
+    (else (repeat (cdr s)))))
+
+;; function test cases
+(define repeat-test
+  (test-suite
+  "tests for repeat function"
+  (check-equal? (repeat '()) #f "empty list")
+  (check-equal? (repeat '(a b b c d e)) #t "repeated list")
+  (check-equal? (repeat '(a b c a b)) #f "non-adjacent repeats")
+  ))
+
+;; run the test cases
+(run-tests repeat-test)
