a2781fa942fb5f296fbf9438c4a01b262e0f6391
coreen <coreeny@cs.washington.edu>
2014-01-08 13:31:08 -0800

    Added unit test cases and a program description.

--- ascending.rkt
+++ ascending.rkt
@@ -1,4 +1,5 @@
 ;; Coreen Yuen
+;; program to test if the elements in a list are in sorted ascending order
 
 ;; returns boolean value true if list of numbers ascending
 ;; empty list and one element list return true, tail-recursive
@@ -7,3 +8,17 @@
         ((eqv? (length x) 1) #t)
         ((not(> (cadr x) (car x))) #f)
         (else (ascending (cdr x)))))
+
+;; unit test cases
+(define ascending-tests
+  (test-suite 
+   "tests for ascending program"
+   (check-equal? (ascending '()) #t "empty list")
+   (check-equal? (ascending '(1)) #t "one element list")
+   (check-equal? (ascending '(1 2 5)) #t "in-order list")
+   (check-equal? (ascending '(2 4 1)) #f "out-of-order list")
+   (check-equal? (ascending '(2 2)) #f "repeat list")
+   ))
+
+;; this line runs the tests
+(run-tests ascending-tests)
